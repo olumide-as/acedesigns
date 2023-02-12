388575fe5b2d42a65363e4481b4b1abec25cacba
olumide-as <adigunsamuel84@gmail.com>
2023-02-12 20:54:17 +0000

    updated about section

--- src/containers/About/About.jsx
+++ src/containers/About/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './About.css';
 import { Heading, BrandSmall} from '../../components';
-import {Instagram, LinkedIn, Twitter} from '../../assets';
+import {Github, Instagram, LinkedIn, Twitter} from '../../assets';
 
 
 const About = () => {
@@ -16,10 +16,10 @@ const About = () => {
         
       <div className="ace__about-text">
         <p>
-        Adigun Olumide Samuel is a graduate of Civil Engineering, a design enthusiat, loves great music and believes there’s a future in the blockchain. He is awesome at Illustrations and Graphic Design,  Frontend Design, a spice of Backend Design, Video Editing and Animation, Structural Engineering Design, Architectural Design and Microsoft Suite Design.
+        Adigun Olumide Samuel is a structural engineer, a design enthusiat, loves great music and believes there’s a future in the blockchain. He is awesome at Illustrations and Graphic Design,  Frontend Design, a spice of Backend Design, Video Editing and Animation, Structural Engineering Design, Architectural Design and Microsoft Suite Design.
         </p>
         <p>
-        He is currently working at both an engineering firm and as a freelance designer at a website that renders design products and services.
+        He is currently studying at Nottingham,England and works as a freelance designer at a website that renders design products and services.
         </p>
         <p>
         Subsequently, he's constantly working to be a better designer in all necessary areas and expand his horizon.
@@ -30,6 +30,7 @@ const About = () => {
       <h4>Follow me on Social Media</h4>
       <a href="https://www.instagram.com/olumide_as/" target="_blank" rel="noopener noreferrer"><BrandSmall brand="@olumide_as" imgUrl={Instagram}/></a>
       <a href="https://twitter.com/olumide_as" target="_blank" rel="noopener noreferrer"><BrandSmall brand="@olumide_as" imgUrl={Twitter}/></a>
+      <a href="https://github.com/olumide-as" target="_blank" rel="noopener noreferrer"><BrandSmall brand="@olumide-as" imgUrl={Github}/></a>
       <a href="https://www.linkedin.com/in/olumide-as/" target="_blank" rel="noopener noreferrer"><BrandSmall brand="Adigun Olumide Samuel" imgUrl={LinkedIn}/></a>
       </div>
 
